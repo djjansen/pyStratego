92e29a0c085a7f4dda093994657c88cb004da4b0
djjansen <ajjansen1818@gmail.com>
2020-10-07 17:27:22 -0400

    resized chat input bar. moved a bunch of js to socket_io.js

--- static/socket_io.js
+++ static/socket_io.js
@@ -1,61 +1,106 @@
-var io = require('socket.io')(80);
-include('"https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"');
+// runs when user connects
+function connect_socket(session_id,user_id) {
+  // send websocket message re: connection
+  socket.emit( 'my event', {
+    data: 'User Connected'
+  } );
+  // listener on server creates new session 
+  console.log('Creating game...');
+  socket.emit('create', {
+    data: session_id
+  });
 
-      var socket = io.connect('http://' + document.domain + ':' + location.port);
-      var session_id = "{{ session.get('session_id') }}";
+  // message handler for the 'join_room' channel
+  socket.on('join_room', function(msg) {
+      console.log(msg);
+  });
 
-      socket.on( 'connect', function() {
-        socket.emit( 'my event', {
-          data: 'User Connected'
-        } );
-
-        console.log('Creating game...');
-        socket.emit('create', {
-          data: '{{ g.session_id }}'
-        });
-
-        // message handler for the 'join_room' channel
-        socket.on('join_room', function(msg) {
-            console.log(msg);
-        });
-
-        var form = $( 'form' ).on( 'submit', function( e ) {
-          e.preventDefault()
-          let user_name = '{{ session.get("user_id") }}'
-          let user_input = $( 'input.message' ).val()
-          socket.emit( 'chat message', {
-            user_name : user_name,
-            message : user_input
-          } )
-          $( 'input.message' ).val( '' ).focus()
-        } )
-      } )
+  // establish input for sending chat messages
+  // chat message listener on server handles emits
+  var form = $( 'form' ).on( 'submit', function( e ) {
+  e.preventDefault()
+  let user_name = user_id
+  let user_input = $( 'input.message' ).val()
+  socket.emit( 'chat message', {
+    user_name : user_name,
+    message : user_input
+  } )
+  $( 'input.message' ).val( '' ).focus()
+  } )
+}
 
 
-      socket.on( 'my response', function( msg ) {
-        console.log( msg )
-        console.log("breakpoint")
-        if( typeof msg.user_name !== 'undefined' ) {
-          $( 'div.message_holder' ).append( '<div><b style="color: #000">'+msg.user_name+'</b> '+msg.message+'</div>' )
-        } else if ( '{{ session.get('color') }}' !== msg['moved_piece']['team'] ) 
-          {
-          console.log( 'yayyyyy')
-          socket.emit( 'board state', msg );
-          var origin_row = msg['origin_cell'][0];
-          var origin_col = msg['origin_cell'][1];
-          var destination_row = msg['destination_cell'][0];
-          var destination_col = msg['destination_cell'][1];
-          console.log(msg);
-          console.log(document.getElementById(destination_row).querySelector('#\\3' + destination_col + ' '));
-          document.getElementById(destination_row).querySelector('#\\3' + destination_col + ' ').classList.add( msg['moved_piece']['team']);
-          var offboard_origin = msg['origin_cell'].includes("");
-          console.log(offboard_origin);
-          if ( offboard_origin  === false) {
-            console.log(msg['origin_cell'].includes(""));
-            console.log('nooooooo');
-            extractData(document.getElementById(origin_row).querySelector('#\\3' + origin_col + ' ').querySelector('.square'));
-          }
+// main function for handling chats & board states
+function handle_incoming_data(msg) {
+  // check if response is chat message
+  if ( typeof msg.user_name !== 'undefined' ) {
+    $( 'div#msg_holder' ).append( '<div class="msg"><b style="color: #000">'+msg.user_name+'</b> '+msg.message+'</div>' );
+    scroll_chat("x");
+  // otherwise handle as board state
+  } else {
+      // actions for both players when a board state is sent from back end
+      if ( msg.game_over === true) { 
+        modifyGrid('grid',modification='removeListeners');
+          show_victory_screen(msg.responsible_team);
+      } else {
+      window.phase = msg['phase'];
+      showPhase(window.phase);
+      if ((window.phase != 'preparation') & (document.getElementById('chest').classList.contains('hidden') === false)) {
+        toggle_element(document.getElementById('chest'));
+      }
+      }
+      // handling of board states received by backend for opposing moves only
+      // modifies front end based on change
+      console.log(msg);
+      if ( window.own_team !== msg['responsible_team'] ) {
+        var origin_row = msg['origin_cell'][0];
+        var origin_col = msg['origin_cell'][1];
+        var destination_row = msg['destination_cell'][0];
+        var destination_col = msg['destination_cell'][1];
+        var team = msg['moved_piece']['color'];
 
-          loadData(document.getElementById(destination_row).querySelector('#\\3' + destination_col + ' ').querySelector('.square'),payload="");
+        if ( team == window.own_team) {
+          var payload = msg['moved_piece']['piece'];
+        } else {
+          var payload = "";
         }
-      })
+  
+        var offboard_origin = msg['origin_cell'].includes("");
+        if ( offboard_origin  === false) {
+          extractData(document.getElementById(origin_row).querySelector('#' + CSS.escape(origin_col)).querySelector('.square').querySelector('.content'));
+        }
+
+        loadData(document.getElementById(destination_row).querySelector('#' + CSS.escape(destination_col)).querySelector('.square').querySelector('.content'),payload=payload,
+          team=team,sync_board=true);
+        socket.emit( 'opposing board sync' );
+              }
+          }
+}
+
+
+// modify grid to highlight valid moves
+function return_range(msg) {
+  console.log(msg['phase']);
+  if (window.phase === window.own_team) {
+    modifyGrid('grid',modification='highlightSquares',movementRange=msg);
+  }
+}
+
+
+// enact front-end changes for your own combats after getting result from server
+function handle_own_combat_result(msg) {
+  if (window.phase === window.own_team) {
+    var destination_row = msg['destination_cell'][0];
+    var destination_col = msg['destination_cell'][1];
+    var winning_team = msg['moved_piece']['color'];
+    if (winning_team === window.own_team) {
+      var payload = msg['moved_piece']['piece'];
+    } else {
+      var payload = "";
+    }
+
+
+    loadData(document.getElementById(destination_row).querySelector('#' + CSS.escape(destination_col)).querySelector('.square').querySelector('.content'),
+      payload=payload,team=winning_team,sync_board=true);
+  }
+}
